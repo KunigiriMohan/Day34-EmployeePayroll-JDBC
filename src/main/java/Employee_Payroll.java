b475c4f6d4beb2562236e0aa9021f63fcaf25ddd
KunigiMohan <kmohanr5@gmail.com>
2021-12-01 07:34:01 +0530

    Added : Ability to retrieve Employee salary and Employee name for database.

--- src/main/java/Employee_Payroll.java
+++ src/main/java/Employee_Payroll.java
@@ -5,15 +5,24 @@ import java.util.Enumeration;
 
 public class Employee_Payroll {
     public static void main(String[] args) {
-        String jdbcurl="jdbc:mysql://localhost:3306/payroll_service?useSSL=false";
+        String jdbcurl="jdbc:mysql://localhost:3306/payroll_service?useSSL=false";                  //storing user details in variable
         String userName="root";
         String password="3006";
         Connection con;
-        try{
+        Statement statement;
+        try{                                                                                    //try catch method to catch any exception occurs
             Class.forName("com.mysql.cj.jdbc.Driver");
             System.out.println("Driver Loaded");
             con = DriverManager.getConnection(jdbcurl, userName, password);
             System.out.println("Connection is Succesful "+con);
+            statement=con.createStatement();
+            ResultSet resultSet=statement.executeQuery("select * from employee_payroll");
+            while(resultSet.next()){                                                            //using while loop printing employee data
+                String name = resultSet.getString("name");
+                String date = resultSet.getString("start_date");
+
+                System.out.print(name+"   "+date);
+            }
         } catch (Exception e) {
             e.printStackTrace();
         }
